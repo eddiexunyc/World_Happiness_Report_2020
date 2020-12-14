91918f38256e33f2bf9724d5eb495b9b165da4e9
Eddie <eddie.xu.nyc@gmail.com>
2020-12-13 19:34:06 -0500

    read json file and print in html

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
-from flask import Flask, render_template, redirect
+import os
+from flask import Flask, render_template, json, current_app as app
 
 app = Flask(__name__)
+
+data_list = []
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("home.html")
+
+@app.route("/data")
+def data():
+    filename = 'data/2015.json'
+    with open(filename, 'r') as jsonfile:
+        data = json.load(jsonfile)
+        for x in data:
+            rank = x['rank']
+            country = x['country']
+            score = x['score']
+            gdp = x['gdp']
+            social_support = x['social_support']
+            life_expectancy = x['life_expectancy']
+            freedom = x['freedom']
+            generosity = x['generosity']
+            corruption = x['corruption']
+            data_list.append([rank, country, score, gdp, social_support, life_expectancy, freedom, generosity, corruption])
+    
+    filename.close()
+
+    return render_template("home.html", data = data_list)
 
 
 if __name__ == "__main__":
