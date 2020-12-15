4bb4374fec3c9aa71727ef46f316e46c05baffdb
Ksenia Dyakova <kseniadyakova@gmail.com>
2020-12-15 16:07:00 -0500

    new index,data,app.py and jupnote mongo file

---
+++ new_files/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+from flask import jsonify
+
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/project_1"
+mongo = PyMongo(app)
+
+#Route for index.html (blank for now)
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# Route to render data.html template using data from Mongo
+@app.route('/data')
+def data():
+    project_1 = [doc for doc in mongo.db.collection.find()]
+    print(project_1)
+    #for row in project_1:
+    #    print(row)
+    return render_template('data.html', project_1=project_1)
+
+    #leave this, want to ask about this in class
+    #project_1 = [doc for doc in mongo.db.collection.find()]
+    #print(project_1)
+    #return jsonify(project_1)
+    #give error: TypeError: Object of type 'ObjectId' is not JSON serializable
+
+if __name__ == "__main__":
+    app.run(debug=True)
