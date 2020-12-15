c84e4a055b4b8da5f63f6e8ac8c7c2422846ac36
Patrick Saitta <ptsaitta@gmail.com>
2020-12-14 20:59:53 -0500

    created proof of concept radar chart

--- radar_chart/static/js/radar_chart.js
+++ radar_chart/static/js/radar_chart.js
@@ -1,21 +1,30 @@
 
+//proof of concept with switzerland
+
+//data is structured: [score1, score2, score3, score4, score5, score6] total is 7.59
+var Switzerland = [1.4, 1.35, 0.94, 0.67, 0.3, 0.42];
+
+
+
 
 var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
+var mychart = new Chart(ctx, {
     // The type of chart we want to create
     type: 'radar',
 
     // The data for our dataset
     data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        labels: ['score1', 'score2', 'score3', 'score4', 'score5', 'score6'],
         datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
+            label: "Baby's First dataset (Switzerland)'",
+            backgroundColor: 'rgb(126, 237, 148)',
+            borderColor: 'rgb(82, 186, 103)',
+            data: Switzerland
         }]
     },
 
     // Configuration options go here
     options: {}
 });
+
+// rbg(255, 99, 132)
