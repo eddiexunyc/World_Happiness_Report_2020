66bf69bbd47880be7c6bb3247cb28f65c4907ff9
Eddie <eddie.xu.nyc@gmail.com>
2020-12-16 22:09:36 -0500

    created json obj and updated data.html

--- new_files/app.py
+++ new_files/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, Response
 from flask_pymongo import PyMongo
 from flask import jsonify
 import json
@@ -13,20 +13,31 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/project_1"
 mongo = PyMongo(app)
 
+#write a json file
+def json_file(obj):
+    with open("project_1", "w") as outfile:
+        outfile.write(obj)
+
+
 #Route for index.html (blank for now)
 @app.route("/")
 @app.route("/home")
-@app.route("/home.html")
+@app.route("/index.html")
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
 # Route to render data.html template using data from Mongo
 @app.route('/data')
 def data():
-    #for mac users
     project_1 = [doc for doc in mongo.db.collection.find()]
-    #print(project_1)
+    return render_template("data.html", project_1 = project_1)
+    
+
+@app.route('/getMyJson')
+def getMyJson():
+    project_1 = [doc for doc in mongo.db.collection.find()]
     return json_util.dumps(project_1)
 
 if __name__ == "__main__":
+    json_file(getMyJson())
     app.run(debug=True)
